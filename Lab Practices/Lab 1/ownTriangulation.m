42876994136db0bd14782512a51febe33069ff04
marcpages2020 <47534017+marcpages2020@users.noreply.github.com>
2019-10-14 18:18:05 +0200

    forward started

--- Lab Practices/Lab 1/ownTriangulation.m
+++ Lab Practices/Lab 1/ownTriangulation.m
@@ -8,6 +8,16 @@ function [At, bt]= ownTriangulation(A,b)
 % Outputs:
 %	At: Upper triangular matrix
 % 	bt: Modified independent term
-
-
+[index,~] = size(b);
+At = A;
+bt = b;
+for i=1:1:index-1
+ for j=i:1:index-1
+ bt(j+1)=b(j+1)*A(i,1)- b(i)*A(j+1,1);
+ At(j+1,:) = A(j+1,:)*A(i,1) - A(i,:)*A(j+1,1);
+ disp(At);
+ disp(bt);
+ end
+ A = At;
+ b = bt;
 end
