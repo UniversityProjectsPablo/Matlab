f5a1febb6bb138e18b361c5855bd64f1dae068b8
marcpages2020 <47534017+marcpages2020@users.noreply.github.com>
2019-10-15 21:04:39 +0200

    base for partial pivoting using ownTriangulation

--- Lab Practices/Lab 1/ownTriangulationPartialPivoting.m
+++ Lab Practices/Lab 1/ownTriangulationPartialPivoting.m
@@ -11,7 +11,17 @@ function [flag, At, bt]= ownTriangulationPartialPivoting(A,b)
 %	      Flag = 1 -> The system of LE has infinite or has not a solution		 
 %	At: Upper triangular matrix
 % 	bt: Modified independent term
-
+[index,~] = size(b);
+At = A;
+bt = b;
+for i=1:1:index
+ for j=i+1:1:index
+ bt(j)=(bt(i)*At(j,i)-bt(j)*At(i,i));
+ At(j,:) = At(i,:)*At(j,i) - At(j,:)*At(i,i);
+ disp(At);
+ disp(bt);
+ end
+end
 
 
 end
