7cf990b6d67d6cb055eda5477ba8c461c9187c73
marcpages2020 <47534017+marcpages2020@users.noreply.github.com>
2019-10-16 19:17:20 +0200

    comments added

--- Lab Practices/Lab 1/backSubs.m
+++ Lab Practices/Lab 1/backSubs.m
@@ -9,13 +9,15 @@ function [x] = backSubs(A,b)
 %	x: Solution of A*x = b
 
 index = size(b,1); %first que get the size of the matrix
-x = zeros(1,index); %we 
+x = zeros(1,index); %we make a vector full of zeroes 
 
-x(index) = b(index)/A(index,index);
+x(index) = b(index)/A(index,index); %we find the solutuion of the last variable
 
     for i = index-1:-1:1
-    x(i) = (b(i)-dot(A(i,:),x))/A(i,i);  
+    x(i) = (b(i)-dot(A(i,:),x))/A(i,i); 
     disp(x);
+    %once we find the solution que can 
+    %substitute and iteratively find the other solutions
     end
 end
 
