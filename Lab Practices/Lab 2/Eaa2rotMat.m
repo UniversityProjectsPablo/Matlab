4cb07ccc3186429ce72bb0d219c7ad3a108b4fa3
marcpages2020 <47534017+marcpages2020@users.noreply.github.com>
2019-11-12 18:18:51 +0100

    initial files

---
+++ Lab Practices/Lab 2/Eaa2rotMat.m
@@ -0,0 +1,19 @@
+function [R] = Eaa2rotMat(a,u)
+% [R] = Eaa2rotMat(a,u)
+% Computes the rotation matrix R given an angle and axis of rotation. 
+% Inputs:
+%	a: angle of rotation
+%	u: axis of rotation 
+% Outputs:
+%	R: generated rotation matrix
+
+u = u/sqrt(u'*u);
+
+[ux] = [0, -u(3), u(2);
+        u(3), 0, -u(1);
+        -u(2) u(1) 0];
+
+R = eye(3)*cosd(a) + (1 - cosd(a))*(u *u') + ux*sind(a);
+
+end
+
