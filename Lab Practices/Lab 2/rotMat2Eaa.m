f3ff97d6e222640b169f16f4b9cba2740098d0bd
marcpages2020 <47534017+marcpages2020@users.noreply.github.com>
2019-11-22 21:46:40 +0100

    just 5 to be checked

--- Lab Practices/Lab 2/rotMat2Eaa.m
+++ Lab Practices/Lab 2/rotMat2Eaa.m
@@ -7,9 +7,12 @@ function [a,u] = rotMat2Eaa(R)
 %	a: angle of rotation
 %	u: axis of rotation 
 
-a = acosd((trace(R)-1)/2);
-ux = (R - R')/2*sind(a);
-u = [ux(3,2), ux(1,3),ux(2,1)];
-
+a = acos((trace(R)-1)/2);
+    if sin(a) == 0
+    u = (rand(3,1)-.5);
+    else
+    ux = (R - R')/(2*sin(a));
+    u = [ux(3,2), ux(1,3),ux(2,1)];
+    end
 end
 
