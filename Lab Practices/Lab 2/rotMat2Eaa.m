4932a41dd37bed8b4b1c3950baafebcec82394d1
Pablo <eduinvestor100@gmail.com>
2019-11-24 22:36:55 +0100

    ex 3c add case

--- Lab Practices/Lab 2/rotMat2Eaa.m
+++ Lab Practices/Lab 2/rotMat2Eaa.m
@@ -8,16 +8,19 @@ function [a,u] = rotMat2Eaa(R)
 %	u: axis of rotation 
 
     a = acos((trace(R)-1)/2);
+    
     if round(sin(a),5) == 0
         if a == pi
-         ux = (R - eye(3)) / 2;
-         u = [ux(3,2), ux(1,3),ux(2,1)];
+        	ux = (R - eye(3)) / 2;
+        	u = [ux(3,2), ux(1,3),ux(2,1)];
         elseif a == 0
-         u = (rand(3,1)-.5);
+        	u = (rand(3,1));
+        elseif a == 180
+        	u = (rand(3,1));
         end
     else
-    ux = (R - R')/(2*sin(a));
-    u = [ux(3,2), ux(1,3),ux(2,1)];
+        ux = (R - R')/(2*sin(a));
+        u = [ux(3,2), ux(1,3),ux(2,1)];
     end
 end
 
